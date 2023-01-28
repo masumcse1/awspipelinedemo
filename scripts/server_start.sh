c643f6cba758d3c6b5aacc48f74f5766dbdd643b
masumcse1 <optinfra.masum@gmail.com>
2023-01-29 01:28:24 +0600

    correction soled.

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /opt/apache-tomcat-9.0.71/bin/./startup.sh
-sudo /opt/apache-tomcat-9.0.71/bin/./startup.sh
+chmod +x /opt/apache-tomcat-9.0.71/bin/./startup.sh
+/opt/apache-tomcat-9.0.71/bin/./startup.sh
--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo chmod +x /opt/apache-tomcat-9.0.71/bin/./shutdown.sh
-sudo /opt/apache-tomcat-9.0.71/bin/./shutdown.sh
+chmod +x /opt/apache-tomcat-9.0.71/bin/./shutdown.sh
+/opt/apache-tomcat-9.0.71/bin/./shutdown.sh
