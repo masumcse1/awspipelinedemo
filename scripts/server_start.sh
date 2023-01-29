c5285ceefbcf0f2ffaa04cdeede94e019bca194c
masumcse1 <optinfra.masum@gmail.com>
2023-01-29 15:57:31 +0600

    hhhhhh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod +x /opt/apache-tomcat-9.0.71/bin/./startup.sh
-/opt/apache-tomcat-9.0.71/bin/./startup.sh
+chmod +x /opt/tomcat/bin/./startup.sh
+/opt/tomcat/bin/./startup.sh
--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod +x /opt/apache-tomcat-9.0.71/bin/./shutdown.sh
-/opt/apache-tomcat-9.0.71/bin/./shutdown.sh
+chmod +x /opt/tomcat/bin/./shutdown.sh
+/opt/tomcat/bin/./shutdown.sh
